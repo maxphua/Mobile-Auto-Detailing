ffe91f3ebc25f10deb76fdd598ad94d7758b51da
Max Phua <maxphua90@gmail.com>
2021-07-16 15:00:33 +0800

    update 2

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home, login, register
+from .views import home, loginPage, register, product_one
 
 app_name = 'core'
 
 urlpatterns = [
     path('', home, name='home'),
-    path('login/', login, name='login'),
-    path('register/', register, name='register')
+    path('login/', loginPage, name='login'),
+    path('register/', register, name='register'),
+    path('product_one/', product_one, name='product_one'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,16 +1,32 @@
 from django.contrib import auth
-from django.http import response
+from django.http import response, HttpResponse
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 
 def home(request):
     return render(request, 'home_page.html')
 
 
-def login(request):
+def product_one(request):
+    return render(request, 'product/product1.html')
+
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Incorrect username or passworh')
     context = {}
-    return render(request, 'login.html', context)
+    return render(request, 'account/login.html', context)
 
 
 def register(request):
@@ -18,7 +34,10 @@ def register(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for' + '' + user)
         return redirect('/')
     else:
         form = RegisterForm()
-    return render(request, "register.html", {"form": form})
+    context = {"form": form}
+    return render(request, "account/register.html", context)
