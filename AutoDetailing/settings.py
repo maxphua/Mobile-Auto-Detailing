d04319cb0e2b1dbded5a606fefa166a463f9c833
Max Phua <maxphua90@gmail.com>
2021-08-10 21:54:41 +0800

    static3

--- AutoDetailing/settings.py
+++ AutoDetailing/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_in_env')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 # allauth
