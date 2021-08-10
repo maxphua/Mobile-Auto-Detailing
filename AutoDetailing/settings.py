c947a467f36a834d9d81b5be51eb1a231ab3b00f
Max Phua <maxphua90@gmail.com>
2021-08-10 22:30:58 +0800

    static

--- AutoDetailing/settings.py
+++ AutoDetailing/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
