3b6cda29f6ca956f72b8600e4e8b95e3fd40ad8d
Max Phua <maxphua90@gmail.com>
2021-08-11 16:02:32 +0800

    static 1

--- AutoDetailing/settings.py
+++ AutoDetailing/settings.py
@@ -133,13 +133,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
